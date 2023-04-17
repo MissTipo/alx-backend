ac0e92f5c62f1c5c4de4e88f0e3ae48df03843b7
MissTipo <dorine.a.tipo@gmail.com>
2023-04-17 11:49:28 +0300

    Deletion proof hypermedia pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+
+"""
+
+import csv
+import math
+from typing import List, Dict, Union
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Returns all items from dataset when certain rows are
+        removed from the dataset
+        """
+        assert index < 1000, "index must be within range"
+
+        dataset = self.indexed_dataset()
+        end_index = index + page_size
+        next_index = end_index + 1
+        data = []
+        """
+            {apple: 100, mango: 40, papaya: 200}
+
+            """
+        for i in range(index, end_index):
+            if i in dataset.keys():
+                data.append(dataset[i])
+            else:
+                end_index += 1
+
+        page = index // page_size + 1
+        # start_index = (page - 1) * page_size
+        # page_size = current page size
+        return {
+            "index": index,
+            "next_index": next_index,
+            "page_size": page_size,
+            "data": data}
