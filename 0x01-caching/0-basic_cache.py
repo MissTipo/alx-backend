be0999f5aca5ac4058ca6842a96b634649907ca4
MissTipo <dorine.a.tipo@gmail.com>
2023-04-19 06:12:04 +0300

    basic cache dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Basic cache dictionary"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Inherits from BaseCaching and is a caching system"""
+    def put(self, key, item):
+        if key or item:
+            self.cache_data[key] = item
+    def get(self, key):
+        """Returns the dic value linked to the key"""
+        if not key:
+            return None
+        return self.cache_data.get(key)
