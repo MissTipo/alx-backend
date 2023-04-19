15428692d2e577211aeeaf19bc61467858244f56
MissTipo <dorine.a.tipo@gmail.com>
2023-04-19 07:12:15 +0300

    fix pycodestyle

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,9 +5,11 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """Inherits from BaseCaching and is a caching system"""
+
     def put(self, key, item):
         if key or item:
             self.cache_data[key] = item
+
     def get(self, key):
         """Returns the dic value linked to the key"""
         if not key:
