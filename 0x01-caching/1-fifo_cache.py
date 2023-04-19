a73c9f00f270a516eb3d6111df343e4ac35f9fd8
MissTipo <dorine.a.tipo@gmail.com>
2023-04-19 11:37:51 +0300

    change the condition check if key or item exists

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -5,22 +5,23 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    """Inherits from BaseCaching and is a caching system"""
     def __init__(self):
         """Inherits from BaseCaching and is a caching system"""
         super().__init__()
+        self.keys = []
 
     def put(self, key, item):
         """Assigns the item value for the key key to the cahe dictionary"""
-        if key or item:
+        if key and item:
             self.cache_data[key] = item
         # Note: The dictionary is alphabetiaclly sorted
         # So first element to go in is first in the dict
-        elif len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_in = list(self.cache_data.keys())[0]
-            self.cache_data.pop(first_in)
-            print("DISCARD:{}".format(first_in))
-        else:
-            return
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_in = list(self.cache_data.keys())[0]
+                self.cache_data.pop(first_in)
+                print("DISCARD: {}".format(first_in))
+        return
 
     def get(self, key):
         """Returns the dict value linked to key"""
